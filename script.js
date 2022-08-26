2f78e4a4b5bf445f30ab81962934b906bfe9013f
faustyna77 <110495453+faustyna77@users.noreply.github.com>
2022-08-26 22:39:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var todays_date = new Date();
+document.getElementById("dataTime").innerHTML = todays_date;
+ 
+
+onclick=document.getElementById("klasa").style.color='red'
+
+function mouseDown() {
+    document.getElementById("text1").style.color = "red";
+    }
+    function mouseUp() {
+    document.getElementById("text1").style.color = "green";
+    }
+    function mouseOver() {
+    document.getElementById("text2").style.fontSize = "20px";
+    }
+    function mouseOut() {
+    document.getElementById("text2").style.fontSize= "16px";
+    }
